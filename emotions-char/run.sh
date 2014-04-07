1bbb96416b59e671c043d087688e67bf1a468716
Sarah Morash <morash@bluenose.cs.dal.ca>
2014-04-07 13:16:44 -0300

    Updated all profiles with 10 song lyrics each (not including the first song lyrics in each profile to begin with)

--- emotions-char/run.sh
+++ emotions-char/run.sh
@@ -8,3 +8,124 @@
 ./createCharProfile.pl 3 50 love.txt < ../emotion_lyric/love/my_heart_will_go_on.txt
 ./createCharProfile.pl 3 50 sad.txt < ../emotion_lyric/sad/whiskey_lullaby.txt
 ./createCharProfile.pl 3 50 victorious.txt < ../emotion_lyric/victorious/we_are_the_champions.txt
+
+#alive
+./updateCharProfile.pl ../lyrics/alive-lyric/1.txt alive
+./updateCharProfile.pl ../lyrics/alive-lyric/2.txt alive
+./updateCharProfile.pl ../lyrics/alive-lyric/3.txt alive
+./updateCharProfile.pl ../lyrics/alive-lyric/4.txt alive
+./updateCharProfile.pl ../lyrics/alive-lyric/5.txt alive
+./updateCharProfile.pl ../lyrics/alive-lyric/6.txt alive
+./updateCharProfile.pl ../lyrics/alive-lyric/7.txt alive
+./updateCharProfile.pl ../lyrics/alive-lyric/8.txt alive
+./updateCharProfile.pl ../lyrics/alive-lyric/9.txt alive
+./updateCharProfile.pl ../lyrics/alive-lyric/10.txt alive
+
+#angry
+./updateCharProfile.pl ../lyrics/angry-lyric/1.txt angry
+./updateCharProfile.pl ../lyrics/angry-lyric/2.txt angry
+./updateCharProfile.pl ../lyrics/angry-lyric/3.txt angry
+./updateCharProfile.pl ../lyrics/angry-lyric/4.txt angry
+./updateCharProfile.pl ../lyrics/angry-lyric/5.txt angry
+./updateCharProfile.pl ../lyrics/angry-lyric/6.txt angry
+./updateCharProfile.pl ../lyrics/angry-lyric/7.txt angry
+./updateCharProfile.pl ../lyrics/angry-lyric/8.txt angry
+./updateCharProfile.pl ../lyrics/angry-lyric/9.txt angry
+./updateCharProfile.pl ../lyrics/angry-lyric/10.txt angry
+
+#crazy
+./updateCharProfile.pl ../lyrics/crazy-lyric/1.txt crazy
+./updateCharProfile.pl ../lyrics/crazy-lyric/2.txt crazy
+./updateCharProfile.pl ../lyrics/crazy-lyric/3.txt crazy
+./updateCharProfile.pl ../lyrics/crazy-lyric/4.txt crazy
+./updateCharProfile.pl ../lyrics/crazy-lyric/5.txt crazy
+./updateCharProfile.pl ../lyrics/crazy-lyric/6.txt crazy
+./updateCharProfile.pl ../lyrics/crazy-lyric/7.txt crazy
+./updateCharProfile.pl ../lyrics/crazy-lyric/8.txt crazy
+./updateCharProfile.pl ../lyrics/crazy-lyric/9.txt crazy
+./updateCharProfile.pl ../lyrics/crazy-lyric/10.txt crazy
+
+#fear
+./updateCharProfile.pl ../lyrics/fear-lyric/1.txt fear
+./updateCharProfile.pl ../lyrics/fear-lyric/2.txt fear
+./updateCharProfile.pl ../lyrics/fear-lyric/3.txt fear
+./updateCharProfile.pl ../lyrics/fear-lyric/4.txt fear
+./updateCharProfile.pl ../lyrics/fear-lyric/5.txt fear
+./updateCharProfile.pl ../lyrics/fear-lyric/6.txt fear
+./updateCharProfile.pl ../lyrics/fear-lyric/7.txt fear
+./updateCharProfile.pl ../lyrics/fear-lyric/8.txt fear
+./updateCharProfile.pl ../lyrics/fear-lyric/9.txt fear
+./updateCharProfile.pl ../lyrics/fear-lyric/10.txt fear
+
+#free
+./updateCharProfile.pl ../lyrics/free-lyric/1.txt free
+./updateCharProfile.pl ../lyrics/free-lyric/2.txt free
+./updateCharProfile.pl ../lyrics/free-lyric/3.txt free
+./updateCharProfile.pl ../lyrics/free-lyric/4.txt free
+./updateCharProfile.pl ../lyrics/free-lyric/5.txt free
+./updateCharProfile.pl ../lyrics/free-lyric/6.txt free
+./updateCharProfile.pl ../lyrics/free-lyric/7.txt free
+./updateCharProfile.pl ../lyrics/free-lyric/8.txt free
+./updateCharProfile.pl ../lyrics/free-lyric/9.txt free
+./updateCharProfile.pl ../lyrics/free-lyric/10.txt free
+
+#happy
+./updateCharProfile.pl ../lyrics/happy-lyric/1.txt happy
+./updateCharProfile.pl ../lyrics/happy-lyric/2.txt happy
+./updateCharProfile.pl ../lyrics/happy-lyric/3.txt happy
+./updateCharProfile.pl ../lyrics/happy-lyric/4.txt happy
+./updateCharProfile.pl ../lyrics/happy-lyric/5.txt happy
+./updateCharProfile.pl ../lyrics/happy-lyric/6.txt happy
+./updateCharProfile.pl ../lyrics/happy-lyric/7.txt happy
+./updateCharProfile.pl ../lyrics/happy-lyric/8.txt happy
+./updateCharProfile.pl ../lyrics/happy-lyric/9.txt happy
+./updateCharProfile.pl ../lyrics/happy-lyric/10.txt happy
+
+#lonely
+./updateCharProfile.pl ../lyrics/lonely-lyric/1.txt lonely
+./updateCharProfile.pl ../lyrics/lonely-lyric/2.txt lonely
+./updateCharProfile.pl ../lyrics/lonely-lyric/3.txt lonely
+./updateCharProfile.pl ../lyrics/lonely-lyric/4.txt lonely
+./updateCharProfile.pl ../lyrics/lonely-lyric/5.txt lonely
+./updateCharProfile.pl ../lyrics/lonely-lyric/6.txt lonely
+./updateCharProfile.pl ../lyrics/lonely-lyric/7.txt lonely
+./updateCharProfile.pl ../lyrics/lonely-lyric/8.txt lonely
+./updateCharProfile.pl ../lyrics/lonely-lyric/9.txt lonely
+./updateCharProfile.pl ../lyrics/lonely-lyric/10.txt lonely
+
+#love
+./updateCharProfile.pl ../lyrics/love-lyric/1.txt love
+./updateCharProfile.pl ../lyrics/love-lyric/2.txt love
+./updateCharProfile.pl ../lyrics/love-lyric/3.txt love
+./updateCharProfile.pl ../lyrics/love-lyric/4.txt love
+./updateCharProfile.pl ../lyrics/love-lyric/5.txt love
+./updateCharProfile.pl ../lyrics/love-lyric/6.txt love
+./updateCharProfile.pl ../lyrics/love-lyric/7.txt love
+./updateCharProfile.pl ../lyrics/love-lyric/8.txt love
+./updateCharProfile.pl ../lyrics/love-lyric/9.txt love
+./updateCharProfile.pl ../lyrics/love-lyric/10.txt love
+
+#sad
+./updateCharProfile.pl ../lyrics/sad-lyric/1.txt sad
+./updateCharProfile.pl ../lyrics/sad-lyric/2.txt sad
+./updateCharProfile.pl ../lyrics/sad-lyric/3.txt sad
+./updateCharProfile.pl ../lyrics/sad-lyric/4.txt sad
+./updateCharProfile.pl ../lyrics/sad-lyric/5.txt sad
+./updateCharProfile.pl ../lyrics/sad-lyric/6.txt sad
+./updateCharProfile.pl ../lyrics/sad-lyric/7.txt sad
+./updateCharProfile.pl ../lyrics/sad-lyric/8.txt sad
+./updateCharProfile.pl ../lyrics/sad-lyric/9.txt sad
+./updateCharProfile.pl ../lyrics/sad-lyric/10.txt sad
+
+#victorious
+./updateCharProfile.pl ../lyrics/victorious-lyric/1.txt victorious
+./updateCharProfile.pl ../lyrics/victorious-lyric/2.txt victorious
+./updateCharProfile.pl ../lyrics/victorious-lyric/3.txt victorious
+./updateCharProfile.pl ../lyrics/victorious-lyric/4.txt victorious
+./updateCharProfile.pl ../lyrics/victorious-lyric/5.txt victorious
+./updateCharProfile.pl ../lyrics/victorious-lyric/6.txt victorious
+./updateCharProfile.pl ../lyrics/victorious-lyric/7.txt victorious
+./updateCharProfile.pl ../lyrics/victorious-lyric/8.txt victorious
+./updateCharProfile.pl ../lyrics/victorious-lyric/9.txt victorious
+./updateCharProfile.pl ../lyrics/victorious-lyric/10.txt victorious
+
